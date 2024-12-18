42453f5426bb20cb1b51e9d89c8ecc9f2abb69c4
iniV2k <vini.silvaa2002@gmail.com>
2024-12-18 12:27:27 -0300

    feat: implementar lógica da opção de listar tarefas e outros ajustes menores

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,11 +18,11 @@ public class Program {
 		
 		while (rodando) {
 			
-			System.out.println("=== Gerenciador de Tarefas ===");
-			System.out.println("1. Adicionar Tarefa");
-			System.out.println("2. Listas Tarefa");
-			System.out.println("3. Marcar Tarefa");
-			System.out.println("4. Excluir Tarefa");
+			System.out.println("=== Gerenciador de tarefas ===");
+			System.out.println("1. Adicionar tarefa");
+			System.out.println("2. Listar tarefas");
+			System.out.println("3. Marcar tarefa como concluída");
+			System.out.println("4. Excluir tarefa");
 			System.out.println("5. Sair");
 			System.out.print("Escolha uma opção: ");
 			
@@ -31,13 +31,19 @@ public class Program {
 			
 			switch (opcao) {
 			case 1:
-				System.out.print("Digite o título da tarefa: ");
+				System.out.print("\nDigite o título da tarefa: ");
 				String titulo = sc.nextLine();
-				tarefas.add(new Tarefa(id++, titulo, "Pendente"));
-				System.out.println("Tarefa adicionada com sucesso!");
+				tarefas.add(new Tarefa(id++, titulo, "pendente"));
+				System.out.println("\nTarefa '" + titulo + "' adicionada com sucesso!\n");
 				break;
 			case 2:
-				//
+				if (!tarefas.isEmpty()) {
+					System.out.println();
+					tarefas.forEach(System.out::println);
+					System.out.println();
+				} else {
+					System.out.println("\nLista de tarefas está vazia.\n");
+				}
 				break;
 			case 3:
 				//
