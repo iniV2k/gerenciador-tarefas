da68a71547988550978463c37855717c7b384c74
iniV2k <vini.silvaa2002@gmail.com>
2024-12-18 11:52:44 -0300

    chore: criar lista de tarefas e estrutura principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,53 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import util.Tarefa;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		boolean rodando = true;
+		List<Tarefa> tarefas = new ArrayList<>();
 		
-		System.out.println("=== Gerenciador de Tarefas ===");
-		System.out.println("1. Adicionar Tarefa");
-		System.out.println("2. Listas Tarefa");
-		System.out.println("3. Marcar Tarefa");
-		System.out.println("4. Excluir Tarefa");
-		System.out.println("5. Sair");
-		System.out.print("Escolha uma opção: ");
+		while (rodando) {
+			System.out.println("=== Gerenciador de Tarefas ===");
+			System.out.println("1. Adicionar Tarefa");
+			System.out.println("2. Listas Tarefa");
+			System.out.println("3. Marcar Tarefa");
+			System.out.println("4. Excluir Tarefa");
+			System.out.println("5. Sair");
+			System.out.print("Escolha uma opção: ");
+			
+			int opcao = sc.nextInt();
+			
+			switch (opcao) {
+			case 1:
+				//
+				break;
+			case 2:
+				//
+				break;
+			case 3:
+				//
+				break;
+			case 4:
+				//
+				break;
+			case 5:
+				//
+				break;
+			default:
+				//
+				break;
+			}
+			
+		}
 		
 		sc.close();
 	}
